fe0f2be72cac21c24a1be345ee956e8564839c20
Nan <asus.bg87@gmail.com>
2023-08-23 13:17:03 +0700

    midterm

--- que.js
+++ que.js
@@ -8,19 +8,17 @@ app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/que1/:number', function (req, res){
     let number = req.params.number
-    for (let i = 1; i <= number; i++) {
-        res.send('number: ' + i)
-    }
-    //res.send('number: ' + number)
-
+    const x = number--
+    res.send('number: '+ number + '-- = ' + x)
 })
 
 app.get('/que2/:number', function (req, res){
     let number = req.params.number
+    let fact = 1
     for (let i = 1; i <= number; i++) {
-        res.send('number: ' + i)
+        fact *= i;
     }
-    //res.send('number: ' + number)
+    res.send('number: ' + number + '! = ' + fact)
 
 })
 
